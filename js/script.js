325cb290680863a2d66b7eb0860e815610e759c0
Marcin Grajdek <grajdek.m@vp.pl>
2022-02-07 13:05:08 +0100

    Add js - gra zd 2

--- js/script.js
+++ js/script.js
@@ -1,8 +1,65 @@
-let computerMove = `kamień`;
+//GRA
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+//Computer
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-let playerMove = 'papier';
+console.log('Wylosowana liczba to: ' + randomNumber);
 
-printMessage('Zagrałem' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){ computerMove = 'kamień';} 
+else if(randomNumber == 2){ computerMove = 'papier';}
+else if(randomNumber == 3){ computerMove = 'nożyce';} 
+    
+printMessage('Mój ruch to: ' + computerMove);
+
+//Player
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){ playerMove = 'kamień';}
+else if(playerInput == '2'){ playerMove = 'papier';}
+else if(playerInput == '3'){ playerMove = 'nożyce';}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+//Result
+//you win
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+  }
+else if(computerMove == 'papier' && playerMove == 'nożyce'){ 
+    printMessage('Ty wygrywasz!');
+}
+else if(computerMove == 'nożyce' && playerMove == 'kamień'){ 
+    printMessage('Ty wygrywasz');
+}
+//remis
+else if(computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Remis!');
+}
+else if(computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis!');
+}
+else if(computerMove == 'nożyce' && playerMove == 'nożyce'){
+    printMessage('Remis!');
+}
+//computer win
+else if( computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Komputer wygrywa!');
+  }
+else if(computerMove == 'nożyce' && playerMove == 'papier'){ 
+    printMessage('Komputer wygrywa!');
+}
+else if(computerMove == 'kamień' && playerMove == 'nozyce'){ 
+    printMessage('Komputer wygrywa');
+}
+//Validate
+else { 
+    printMessage('Spróbuj ponownie: wybierz cyfrę 1 lub 2 lub 3.');
+}
+
+
 
