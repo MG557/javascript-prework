7730a7b29ef1f16a9395a7a95057296721a992b9
Marcin Grajdek <grajdek.m@vp.pl>
2022-02-08 11:13:52 +0100

    Add gra wersja robocza 1

--- js/script.js
+++ js/script.js
@@ -33,7 +33,8 @@ printMessage('Mój ruch to: ' + computerMove);
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = getMoveName(randomNumber);
+let playerMove = getMoveName(playerInput);
+
 /*
 if(playerInput == '1'){ playerMove = 'kamień';}
 else if(playerInput == '2'){ playerMove = 'papier';}
@@ -42,7 +43,32 @@ else if(playerInput == '3'){ playerMove = 'nożyce';}
 printMessage('Twój ruch to: ' + playerMove);
 
 //Result
-//you win
+
+let argPlayerMove = getMoveName(playerInput);
+let argComputerMove = getMoveName(randomNumber);
+
+
+function displayResult(argComputerMove, argPlayerMove){
+    printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+  
+    if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
+      printMessage('Ty wygrywasz!');
+    } 
+    else if( argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
+      printMessage('Ty wygrywasz!');
+    }
+    else if( argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
+        printMessage('Ty wygrywasz!');
+      }
+    else if( argComputerMove == 'kamień' && argPlayerMove == 'kamień' + argComputerMove == 'papier' && argPlayerMove == 'papier' + argComputerMove == 'nożyce' && argPlayerMove == 'nożyce'){ 
+        printMessage('Remis.');
+    }
+    else {
+      printMessage('Tym razem przegrywasz :(');
+    }
+  }
+
+/*
 if( computerMove == 'kamień' && playerMove == 'papier'){
     printMessage('Ty wygrywasz!');
   }
@@ -76,6 +102,6 @@ else if(computerMove == 'kamień' && playerMove == 'nozyce'){
 else { 
     printMessage('Spróbuj ponownie: wybierz cyfrę 1 lub 2 lub 3.');
 }
-
+*/
 
 
